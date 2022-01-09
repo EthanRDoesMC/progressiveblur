3cb5aca55f9c5359307570e8ea1f48ad7961176c
Ethan Chaffin <me@ethanrdoesmc.com>
2022-01-09 00:11:36 -0600

    transparent view

--- progressiveblur/ViewController.swift
+++ progressiveblur/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var blurStyle: UISegmentedControl!
 
+    @IBOutlet var colorSegment: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,6 +20,14 @@ class ViewController: UIViewController {
         let detail: UIViewController! = storyboard!.instantiateViewController(withIdentifier: "Detail")
         let transitionDelegate = _UIProgressiveBlurContextController()
         transitionDelegate.blurStyle = blurStyle.selectedSegmentIndex
+        switch colorSegment.selectedSegmentIndex {
+        case 0:
+            break
+        case 1:
+            detail.view.backgroundColor = .clear
+        default:
+            break
+        }
         detail.modalPresentationStyle = .custom
         detail.transitioningDelegate = transitionDelegate
         present(detail, animated: true, completion: nil)
