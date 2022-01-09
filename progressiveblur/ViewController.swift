2b003db27a2f5ed1f064cbb80bdb69732e94b740
Ethan Chaffin <me@ethanrdoesmc.com>
2022-01-08 21:52:41 -0600

    Initial Commit

---
+++ progressiveblur/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  progressiveblur
+//
+//  Created by Ethan Chaffin on 1/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
